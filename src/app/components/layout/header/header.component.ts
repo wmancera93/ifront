425f2489001ad8382c6ee21c4414b280450bf047
Laura Beltrán <laura.beltran@hrinteractive.co>
2018-02-06 17:23:53 -0500

    Maquetación header-modificación en style principal

--- src/app/components/layout/header/header.component.ts
+++ src/app/components/layout/header/header.component.ts
@@ -2,18 +2,25 @@ import { Component, OnInit } from '@angular/core';
 import { UserSharedService } from '../../../services/shared/common/user/user-shared.service';
 import { User } from '../../../models/user';
 
+
+
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
+
+
 export class HeaderComponent implements OnInit {
   private dataUser: User = null;
+  title: string = 'Mis datos';
   constructor(private userSharedService: UserSharedService) {
     this.userSharedService.getUser().subscribe((data) => {
       this.dataUser = data;
     });
+   
   }
+
 
   ngOnInit() {
     this.getDataLocalStorage();
