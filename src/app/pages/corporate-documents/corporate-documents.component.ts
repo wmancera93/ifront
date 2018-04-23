82e1e4d6e8b3c3135ccca1d36f4666dee9bff203
Laura Beltrán <laura.beltran@hrinteractive.co>
2018-04-23 14:52:47 -0500

    edit style from form new article

--- src/app/pages/corporate-documents/corporate-documents.component.ts
+++ src/app/pages/corporate-documents/corporate-documents.component.ts
@@ -3,9 +3,10 @@ import { CorporateDocsService } from '../../services/corporate-documents/corpora
 import { Documents, TypeDocuments } from '../../models/common/corporate_documents/corporate_documents';
 import { RequestOptions, ResponseContentType } from '@angular/http';
 import { DownloadFilesService } from '../../services/download-files/download-files.service';
-import { HttpClient } from '@angular/common/http';
-import { saveAs } from 'file-saver/FileSaver';
+import { saveAs } from 'file-saver/FileSaver.js';
 import { Observable } from 'rxjs/Observable';
+import { Http, Headers } from '@angular/http';
+import { timeout } from 'q';
 
 
 @Component({
@@ -20,29 +21,44 @@ export class CorporateDocumentsComponent implements OnInit {
   public urlDocs: string;
   public urlPDF: any;
   constructor(public corporateDocsService: CorporateDocsService,
-    public downloadFilesService: DownloadFilesService, public http: HttpClient) { }
+    public downloadFilesService: DownloadFilesService, public http: Http) { }
 
-  ngOnInit() {
+  ngOnInit() {    
+   
     this.corporateDocsService.getDocuments().subscribe((data: any) => {
-
       this.infoDocs = data.data[0].documents;
       this.urlDocs = data.data[0].base_url;
       this.docsType = data.data[0].type_documents;
-
     })
 
   }
   downloadPDF(doc: any) {
     this.urlPDF = this.urlDocs + doc.url;
     this.downloadName = doc.name;
-    let FileSaver = require('file-saver');
-    window.open(this.urlPDF, "_blank");
-    //let blob = new Blob(this.urlPDF);
-  //  let file = new File(["Hello, world!"], "hello world.txt", { type: "text/plain;charset=utf-8" });
-  //  FileSaver.saveAs(blob);
-    // this.downloadFilesService.getFile().subscribe((data));
+    const headers = new Headers();
+    headers.append('Accept', 'pdf');
+
+    this.http.get(this.urlPDF).subscribe((data: any) => {
+      console.log(data._body)
+      let FileSaver = require('file-saver');
+      let blob = new Blob([data._body], {type: "application/pdf;charset=utf-8"});
+      FileSaver.saveAs(blob, "hello world.pdf");
+
+      // var file = new File([data._body], "hello world.pdf", { type: "application/pdf;charset=utf-8" });
+      // FileSaver.saveAs(file);
+    })
+    // window.open(this.urlPDF, "_blank");
+
   }
-  
+
+  private saveToFileSystem(response: any) {
+    // const contentDispositionHeader: string = response.headers.get('Content-Disposition');
+    // const parts: string[] = contentDispositionHeader.split(';');
+    const filename = 'Presentacion Corporativa 2016.pdf';
+
+    // saveAs(blob, filename);
+  }
+
 
   showButton(idBtn: string) {
     document.getElementById(idBtn).removeAttribute('style');
