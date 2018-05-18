90ab23ded15815af740d1174b0bf75e1ad1af99c
wilmermancera <wilmer.mancera@hrinteractive.co>
2018-05-18 12:48:03 -0500

    publish https for production

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   apiBaseHr_staging: 'http://apihr-staging.hrinteractive.co',
-  apiBaseHr_production: 'http://apihr.hrinteractive.co',
+  apiBaseHr_production: 'https://apihr.hrinteractive.co',
   apiBaseHr_development: 'http://apihr-development.hrinteractive.co'
 };
