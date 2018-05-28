cb08eba98259c4f4b60ed4054c8fa500f7fc9456
wilmermancera <wilmer.mancera@hrinteractive.co>
2018-05-28 16:23:56 -0500

    version to browser IE 11

--- src/app/components/common/widgets/events-employees/events-employees.component.ts
+++ src/app/components/common/widgets/events-employees/events-employees.component.ts
@@ -32,23 +32,29 @@ export class EventsEmployeesComponent implements OnInit {
         this.nohaveTeam = false;
         this.cauruselIdGeneral = this.objectWidget[0].name_event;
         this.cauruselId = '#' + this.objectWidget[0].name_event;
+
+        setTimeout(() => {
+          if (this.stylesExplorerService.validateBrowser()) {
+            switch (this.cauruselIdGeneral) {
+              case "birthdays":
+                (<HTMLInputElement>document.getElementById(this.cauruselIdGeneral).childNodes[3]).style.width = '330px';
+                break;
+              case "anniversaries":
+                (<HTMLInputElement>document.getElementById(this.cauruselIdGeneral).childNodes[3]).style.width = '330px';
+                break;
+              case "new_employees":
+                (<HTMLInputElement>document.getElementById(this.cauruselIdGeneral).childNodes[3]).style.width = '330px';
+                break;
+              case "my_team":
+                (<HTMLInputElement>document.getElementById(this.cauruselIdGeneral).childNodes[3]).style.width = '500px';
+                break;
+
+              default:
+                break;
+            }
+          }
+        }, 3000);
       };
-      setTimeout(() => {
-        if (this.stylesExplorerService.validateBrowser()) {
-          if (document.getElementById("birthdays") !== null) {
-            (<HTMLInputElement>document.getElementById("birthdays").childNodes[3]).style.width = '330px';
-          }
-          if (document.getElementById("anniversaries") !== null) {
-            (<HTMLInputElement>document.getElementById("anniversaries").childNodes[3]).style.width = '330px';
-          }
-          if (document.getElementById("new_employees") !== null) {
-            (<HTMLInputElement>document.getElementById("new_employees").childNodes[3]).style.width = '330px';
-          }
-          if (document.getElementById("my_team") !== null) {
-            (<HTMLInputElement>document.getElementById("my_team").childNodes[3]).style.width = '500px';
-          }
-        }
-      }, 3000);
 
     })
 
