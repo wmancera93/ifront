dd0464a88968b69d710857853aae161a22c5dfcc
Laura Beltrán <laura.beltran@hrinteractive.co>
2018-06-01 09:31:30 -0500

    environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,6 +6,6 @@
 export const environment = {
   production: false,
   apiBaseHr_staging: 'http://apihr-staging.hrinteractive.co',
-  apiBaseHr_production: 'http://apihr.hrinteractive.co',
+  apiBaseHr_production: 'http://apihr.hrinteractive.co', 
   apiBaseHr_development: 'http://apihr-development.hrinteractive.co'
 };
