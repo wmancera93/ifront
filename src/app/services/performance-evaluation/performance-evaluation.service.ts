684c4bbdb6cfa558ba901286a51789e8b16b1c81
Laura Beltrán <laura.beltran@hrinteractive.co>
2018-07-13 11:39:21 -0500

    center calendar

--- src/app/services/performance-evaluation/performance-evaluation.service.ts
+++ src/app/services/performance-evaluation/performance-evaluation.service.ts
@@ -1,8 +1,14 @@
 import { Injectable } from '@angular/core';
+import { Angular2TokenService } from '../../../../node_modules/angular2-token';
 
 @Injectable()
 export class PerformanceEvaluationService {
 
-  constructor() { }
+  constructor(private tokenService: Angular2TokenService) { }
+
+  getPerformanceEvaluations() {
+    return this.tokenService.get('perfomance_evaluations/index_table')
+      .map((data: any) => data.json());
+  }
 
 }
