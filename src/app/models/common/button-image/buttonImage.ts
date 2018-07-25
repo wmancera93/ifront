fe41437b6aeadec8a93d41c84fad63960a59526e
Laura Beltrán <laura.beltran@hrinteractive.co>
2018-07-25 10:30:55 -0500

    change icons

--- src/app/models/common/button-image/buttonImage.ts
+++ src/app/models/common/button-image/buttonImage.ts
@@ -2,7 +2,8 @@ import { Url } from "../url/url";
 
 export interface ButtonImage {
     title: string,
-    image: Url,
+    icon_primary: string,
+    icon_secundary: string,
     tooltipText: string,
     route: string
 }
