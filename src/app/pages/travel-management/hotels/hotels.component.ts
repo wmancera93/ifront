4a7cedab6b058813391cd7423131d1363e5ddf37
wilmermancera <wilmer.mancera@hrinteractive.co>
2018-08-08 11:36:41 -0500

    #hotels advance

--- src/app/pages/travel-management/hotels/hotels.component.ts
+++ src/app/pages/travel-management/hotels/hotels.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FileUploadService } from '../../../services/shared/common/file-upload/file-upload.service';
 import { FormDataService } from '../../../services/common/form-data/form-data.service';
 import { HotelsService } from '../../../services/travel-management/hotels/hotels.service';
+import { AlertsService } from '../../../services/shared/common/alerts/alerts.service';
 
 @Component({
   selector: 'app-hotels',
@@ -9,14 +10,22 @@ import { HotelsService } from '../../../services/travel-management/hotels/hotels
   styleUrls: ['./hotels.component.css']
 })
 export class HotelsComponent implements OnInit {
-  public objectHotels: any[] = []
-  constructor(public hotelsService: HotelsService) {
-    this.hotelsService.getHotelsByCompany().subscribe(data => {
-      if(data.success){
-        this.objectHotels = data.data[0];
-        console.log(this.objectHotels)
-      }      
-    });
+  public objectHotels: any[] = [];
+  public alertWarning: any[] = [];
+  public hotel_id: string;
+
+  constructor(public hotelsService: HotelsService, public alert: AlertsService) {
+    this.alert.getActionConfirm().subscribe((data: any) => {
+      if (data === 'deletHotel') {
+        let hotels: any;
+        hotels = { hotels: [{ id: this.hotel_id }] }
+
+        this.hotelsService.deleteHotelsByCompany(hotels).subscribe((data: any) => {
+          this.getHotels();
+        })
+      }
+    })
+
   }
 
   ngOnInit() {
@@ -25,16 +34,34 @@ export class HotelsComponent implements OnInit {
       left: 0,
       behavior: 'smooth'
     });
+    this.getHotels();
+  }
+
+  returnBackPage() {
 
   }
 
-  returnBackPage(){
+  viewHotels(test) {
 
   }
 
-  viewHotels(test){
-
+  getHotels() {
+    this.hotelsService.getHotelsByCompany().subscribe(data => {
+      if (data.success) {
+        this.objectHotels = data.data;
+      }
+    });
   }
 
-
+  deleteHotels(hotel: any) {
+    this.hotel_id = hotel.id;
+    this.alertWarning = [{
+      type: 'warning',
+      title: 'Confirmación',
+      message: '¿Desea eliminar el hotel con código #' + hotel.id.toString() + '?',
+      confirmation: true,
+      typeConfirmation: 'deletHotel'
+    }];
+    this.alert.setAlert(this.alertWarning[0]);
+  }
 }
