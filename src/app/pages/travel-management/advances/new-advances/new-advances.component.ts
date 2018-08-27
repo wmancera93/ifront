b06a06de2c5f3a41e1a4ac49195dfc97f87dc38c
Laura Beltrán <laura.beltran@hrinteractive.co>
2018-08-27 11:47:03 -0500

    view advance

--- src/app/pages/travel-management/advances/new-advances/new-advances.component.ts
+++ src/app/pages/travel-management/advances/new-advances/new-advances.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { TravelsService } from '../../../../services/shared/travels/travels.service';
 import { AdvanceSharedService } from '../../../../services/shared/advance-shared/advance-shared.service';
+import { FormGroup, FormBuilder } from '@angular/forms';
+import { AdvancesService } from '../../../../services/travel-management/advances/advances.service';
 
 @Component({
   selector: 'app-new-advances',
@@ -9,16 +11,37 @@ import { AdvanceSharedService } from '../../../../services/shared/advance-shared
 })
 export class NewAdvancesComponent implements OnInit {
   public showSubmit: boolean = true
-  formAdvanceTravel
-  constructor(public advanceSharedService: AdvanceSharedService) {
+  public formAdvanceTravel: any;
+  public listTravelsFromAdvance: any;
+  public listMoneyTypes: any;
+
+  constructor(public advanceSharedService: AdvanceSharedService,
+    public advancesService: AdvancesService,
+    public fb: FormBuilder) {
+
+    this.formAdvanceTravel = new FormGroup({});
+    this.formAdvanceTravel = fb.group({
+      travel_request_id: "",
+      currency_id: "",
+      value: "",
+      date: "",
+      observation: ""
+    });
 
     this.advanceSharedService.getNewAdvance().subscribe((data: any) => {
-   
       if (document.getElementById('advance_new').className !== 'modal show') {
         document.getElementById('btn_advances_new').click();
       }
-
     });
+
+    this.advancesService.getAdvanceListTravel().subscribe((list: any) => {
+      this.listTravelsFromAdvance = list.data;
+    });
+
+    this.advancesService.getAdvanceMoneyList().subscribe((money: any) => {
+      this.listMoneyTypes = money.data;
+    })
+
   }
 
 
@@ -26,8 +49,24 @@ export class NewAdvancesComponent implements OnInit {
   }
 
 
-  newAdvance(param){
-    
+  newAdvance(param) {
+    console.log(param)
+  }
+
+  aditionAdvance(dataAgree)
+  {
+    console.log(dataAgree)
+  }
+  
+  clearFormAdvance()
+  {
+    this.formAdvanceTravel = this.fb.group({
+      travel_request_id: "",
+      currency_id: "",
+      value: "",
+      date: "",
+      observation: ""
+    });
   }
 
 }
---
+++ src/app/pages/travel-management/advances/view-advance/view-advance.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { AdvanceSharedService } from '../../../../services/shared/advance-shared/advance-shared.service';
+import { AdvancesService } from '../../../../services/travel-management/advances/advances.service';
+
+@Component({
+  selector: 'app-view-advance',
+  templateUrl: './view-advance.component.html',
+  styleUrls: ['./view-advance.component.css']
+})
+export class ViewAdvanceComponent implements OnInit {
+  public infoAdvance: any[] = [];
+  constructor(public advanceSharedService: AdvanceSharedService,
+    public advancesService: AdvancesService) {
+    this.advanceSharedService.getViewAdvance().subscribe((id: any) => {
+      this.advancesService.getAdvanceByID(id).subscribe((advance: any) => {
+        if (document.getElementById('modal_viewAdvance').className !== 'modal show') {
+          document.getElementById('btn-viewAdvance').click();
+        }
+        this.infoAdvance = advance.data;
+      })
+
+    })
+
+  }
+
+  ngOnInit() {
+  }
+
+}
