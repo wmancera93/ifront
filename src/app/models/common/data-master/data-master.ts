c10c2409ad266b17363e08c9361dbe4066dc475f
wilmermancera <wilmer.mancera@hrinteractive.co>
2018-09-24 12:07:49 -0500

    change in component dynamic-form to have dynamic selectors and with validation from prerequisites

--- src/app/models/common/data-master/data-master.ts
+++ src/app/models/common/data-master/data-master.ts
@@ -10,3 +10,25 @@ export interface ListDataMaster {
     description: string,
     method_name: string
 }
+
+export class FormDinamic {
+    class_input: string;
+    class_label: string;
+    control: string;
+    id: number;
+    is_prerequisite: false;
+    name_label: string;
+    option: FormDinamicOptions[];
+    order: number;
+    place_holder: string;
+    prerequisite_id: string;
+    required: boolean;
+    type: string;
+    value: string;
+}
+
+export class FormDinamicOptions {
+    code:string;
+    description: string;
+    filtrer?: string;
+}
