1a563609b20d6ffefcc56a4977c8ed798525f361
lizethnunez <lizeth.nsanchez@hrinteactive.co>
2018-10-18 11:38:40 -0500

    p

--- src/app/pages/travel-management/travel-management.component.ts
+++ src/app/pages/travel-management/travel-management.component.ts
@@ -30,42 +30,42 @@ export class TravelManagementComponent implements OnInit {
         title: "Viajes",
         icon_primary: "fa fa-globe",
         icon_secundary: "fa fa-plane",
-        tooltipText: "Haga clic aquí para asignar viajes",
+        tooltipText: "Haga clic aquí para solicitar trayectos, pasajes y hotel",
         route: "/ihr/travels"
       },      
       {
         title: "Anticipos",
         icon_primary: "fa fa-money",
         icon_secundary: "",
-        tooltipText: "Haga clic aquí para asignar anticipos",
+        tooltipText: "Haga clic aquí para solicitar anticipos del viaje",
         route: "/ihr/advances"
       },
       {
         title: "Gastos",
         icon_primary: "fa fa-circle-thin",
         icon_secundary: "fa fa-usd",
-        tooltipText: "Haga clic aquí para asignar gastos",
+        tooltipText: "Haga clic aquí para legalizar los gastos de viaje",
         route: "/ihr/spend"
       },
       {
         title: "Aprobaciones Pendientes",
         icon_primary: "fa fa fa-square-o",
         icon_secundary: "fa fa-clock-o",
-        tooltipText: "Haga clic aquí para asignar aprobaciones",
+        tooltipText: "Haga clic aquí para aprobar trayectos, anticipos o gastos de viajes",
         route: "/ihr/pending_travel"
       },
       {
         title: "Aprobaciones Gestionadas",
         icon_primary: "fa fa fa-square-o",
         icon_secundary: "fa fa-check",
-        tooltipText: "Haga clic aquí para asignar aprobaciones",
+        tooltipText: "Haga clic aquí para ver las solictudes de viajes aprobadas por usted",
         route: "/ihr/management_travel"
       },
       {
         title: "Asignación de hoteles",
         icon_primary: "fa fa-circle-thin",
         icon_secundary: "fa fa-bed",
-        tooltipText: "Haga clic aquí para asignar hoteles",
+        tooltipText: "Haga clic aquí para crear hoteles",
         route: "/ihr/hotels"
       },
     ];
