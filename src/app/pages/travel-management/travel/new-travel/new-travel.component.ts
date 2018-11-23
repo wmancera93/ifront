057fa0d5e42aa9fecc82b0a7972eef1553dba69d
wilmermancera <wilmer.mancera@hrinteractive.co>
2018-11-23 15:00:15 -0500

    d

--- src/app/pages/travel-management/travel/new-travel/new-travel.component.ts
+++ src/app/pages/travel-management/travel/new-travel/new-travel.component.ts
@@ -822,22 +822,22 @@ export class NewTravelComponent implements OnInit, OnDestroy {
   }
   changeTypeTravel(param) {
     console.log(param)
-    if (param.type_travel === '3' || param.type_travel === '16') {
-      this.formTravelManagement.controls['id_travel_legal'].setValue(this.legal_travels.filter(data => data.code === "P")[0].id.toString());
+    if (param.type_travel === 3 || param.type_travel === 16) {
+      this.formTravelManagement.controls['id_travel_legal'].setValue(this.legal_travels.filter(data => data.code === 'P')[0].id.toString());
       this.changeTravelLegal('P');
-      console.log(JSON.stringify(this.legal_travels) + ' - 1')
+      console.log(this.legal_travels + ' - 1')
     } else {
       this.changeTravelLegal('');
     }
-    if (param.type_travel ==='1' || param.type_travel === '3') {
+    if (param.type_travel === 1 || param.type_travel === 3) {
       this.formTravelManagement.controls['id_state'].setValue(this.stateLocations.filter(data => data.code === 'NAL')[0].id.toString());
       this.formTravelManagement.controls['id_stateto'].setValue(this.stateLocationsto.filter(data => data.code === 'NAL')[0].id.toString());
     } else {
       this.formTravelManagement.controls['id_state'].setValue(this.stateLocations.filter(data => data.code === 'INTER')[0].id.toString());
       this.formTravelManagement.controls['id_stateto'].setValue(this.stateLocationsto.filter(data => data.code === 'INTER')[0].id.toString());
     }
-    console.log(JSON.stringify(this.stateLocations) + ' - 2')
-    console.log(JSON.stringify(this.stateLocationsto) + ' - 3')
+    console.log(this.stateLocations + ' - 2')
+    console.log(this.stateLocationsto + ' - 3')
   }
 
   changeTravelLegal(travelLegal: any) {
@@ -864,8 +864,8 @@ export class NewTravelComponent implements OnInit, OnDestroy {
     this.activate = false;
     this.activate_submit = true;
     this.showSubmit = true;
-    // this.stateLocations = [];
-    // this.stateLocationsto = [];
+    this.stateLocations = [];
+    this.stateLocationsto = [];
     this.cityLocations = [];
     this.cityLocationsto = [];
     this.terminalLocations = [];
