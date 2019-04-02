b9b9e0b9ff4e4f4625d066bb53585db09facaa20
wilmer mancerao <wilmer.mancera@hrinteractive.co>
2019-04-02 14:55:18 -0500

    dev

--- server.js
+++ server.js
@@ -1,28 +1,16 @@
-// //Install express server
-// const express = require('express');
-// const path = require('path');
+//Install express server
+const express = require('express');
+const path = require('path');
 
-// const app = express();
+const app = express();
 
-// // Serve only the static files form the dist directory
-// app.use(express.static(__dirname + '/dist'));
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + '/dist'));
 
-// app.get('/*', function(req,res) {
+app.get('/*', function(req,res) {
     
-// res.sendFile(path.join(__dirname+'/dist/index.html'));
-// });
+res.sendFile(path.join(__dirname+'/dist/index.html'));
+});
 
-// // Start the app by listening on the default Heroku port
-// app.listen(process.env.PORT || 8080);
-
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
-
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .get('/cool', (req, res) => res.send(cool()))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
