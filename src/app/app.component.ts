c256b92b5f4caaae0043dce34ce92ded76b79800
wilmer mancerao <wilmer.mancera@hrinteractive.co>
2019-04-03 15:05:12 -0500

    dev

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -68,26 +68,6 @@ export class AppComponent {
       languaje = JSON.parse(localStorage.getItem("treeLanguaje")).data[0].data[0].code_language.toLowerCase();
     }
 
-
-    this.tokenService.init(
-      {
-        apiBase: this.baseUrl,
-        apiPath: 'api/v2/' + languaje,
-        signInPath: 'auth/sign_in',
-        signOutPath: 'auth/sign_out',
-        validateTokenPath: 'auth/validate_token',
-        signOutFailedValidate: false,
-        registerAccountPath: 'auth/password/new',
-        updatePasswordPath: 'auth/password',
-        resetPasswordPath: 'auth/password',
-        globalOptions: {
-          headers: {
-            'Content-Type': 'application/json',
-            'Accept': 'application/json'
-          }
-        }
-      }
-    );
     this.userSharedService.getUser().subscribe((data) => {
       this.dataUser = data;
     });
